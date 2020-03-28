494e0a62ccdcc7155dd2ed406640e2936d3219e1
Hi Tran <thuchitran@gmail.com>
2020-03-28 20:20:24 +0700

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+<div class="row">
+    <div class="col-3 p-5">
+        <img src="https://s3.amazonaws.com/freecodecamp/curriculum-diagram-full.jpg" alt="" class="rounded-circle" style="width: 130px;"/>
+    </div>
+    <div class="col-9 pt-5">
+        <div><h1>freeCodeCamp</h1></div>
+        <div class="d-flex">
+            <div class="pr-5"><strong>153</strong> posts</div>
+            <div class="pr-5"><strong>2.3k</strong> followers</div>
+            <div class="pr-5"><strong>212</strong> following</div>
+        </div>
+    </div>
+</div>
+</div>
+@endsection
