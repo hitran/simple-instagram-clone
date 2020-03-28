64e9a2470e52355501fb1e05c77de5d55d3d897d
Hi Tran <thuchitran@gmail.com>
2020-03-28 20:49:02 +0700

    update view and scss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,18 +2,32 @@
 
 @section('content')
 <div class="container">
-<div class="row">
-    <div class="col-3 p-5">
-        <img src="https://s3.amazonaws.com/freecodecamp/curriculum-diagram-full.jpg" alt="" class="rounded-circle" style="width: 130px;"/>
-    </div>
-    <div class="col-9 pt-5">
-        <div><h1>freeCodeCamp</h1></div>
-        <div class="d-flex">
-            <div class="pr-5"><strong>153</strong> posts</div>
-            <div class="pr-5"><strong>2.3k</strong> followers</div>
-            <div class="pr-5"><strong>212</strong> following</div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://instagram.fhan2-4.fna.fbcdn.net/v/t51.2885-19/s150x150/83213956_3360255157381124_5752385570823208960_n.jpg?_nc_ht=instagram.fhan2-4.fna.fbcdn.net&_nc_ohc=htwGo_NFr5MAX9lrNVl&oh=51c4ed0d36b28f93e0db7492f96b3c01&oe=5EAA5746" alt="" class="rounded-circle"/>
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>freeCodeCamp</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>2.3k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
+            <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
+            <div class="font-weight-bold"><a href="www.freecodecamp.org">www.freecodecamp.org</a></div>
         </div>
     </div>
-</div>
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://instagram.fhan2-4.fna.fbcdn.net/v/t51.2885-15/e35/c165.0.419.419a/91248204_204409004183010_3671867139000667360_n.jpg?_nc_ht=instagram.fhan2-4.fna.fbcdn.net&_nc_cat=106&_nc_ohc=tawAKIj9GAsAX8WktlX&oh=085f74046cb018817f233e86a98c3b48&oe=5EA9557F" class="w-100" alt="">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fhan2-4.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.109.925.925a/s640x640/90429603_618090275709978_2696911208620941912_n.jpg?_nc_ht=instagram.fhan2-4.fna.fbcdn.net&_nc_cat=106&_nc_ohc=mNC8FhfKYvkAX9WSeDr&oh=9a788d4e8c47204f004f88ff9f4d6f9b&oe=5EAA5C0D" class="w-100" alt="">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fhan2-4.fna.fbcdn.net/v/t51.2885-15/e35/c94.0.561.561a/s480x480/89850472_191188785511119_1802925729717063601_n.jpg?_nc_ht=instagram.fhan2-4.fna.fbcdn.net&_nc_cat=103&_nc_ohc=9m5tKpk5rXEAX-vyJjo&oh=a829dd87cf5a4deea17a6209e7ab9b02&oe=5EA75D70" class="w-100" alt="">
+        </div>
+    </div>
 </div>
 @endsection
